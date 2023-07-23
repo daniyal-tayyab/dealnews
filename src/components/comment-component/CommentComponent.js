f0be5a438ab39855d146f5e1b01a2e744e1e8a48
daniyaltayyab <daniyaltayyab91@gmail.com>
2023-07-24 01:57:15 +0500

    dev completed

--- src/components/comment-component/CommentComponent.js
+++ src/components/comment-component/CommentComponent.js
@@ -1,45 +1,23 @@
-import React, { useContext } from "react";
-import { CommentSection } from "react-comments-section";
+import React from "react";
 
-import { UserContext } from "../../context/user.context";
-
-import "react-comments-section/dist/index.css";
+import "./style.css";
 
 const CommentComponent = () => {
-  const { user } = useContext(UserContext);
-  const data = [
-    {
-      userId: "02b",
-      comId: "017",
-      fullName: "Lily",
-      userProfile: "https://www.linkedin.com/in/daniyal-tayyab-77880015a/",
-      text: "I like this product very much!🤔",
-      avatarUrl: "https://ui-avatars.com/api/name=Lily&background=random",
-      replies: [],
-    },
-  ];
   return (
-    <CommentSection
-      currentUser={{
-        currentUserId: "01a",
-        currentUserImg:
-          "https://ui-avatars.com/api/name=Riya&background=random",
-        currentUserProfile:
-          "https://www.linkedin.com/in/daniyal-tayyab-77880015a/",
-        currentUserFullName: user,
-      }}
-      logIn={{
-        loginLink: "http://localhost:3001/",
-        signupLink: "http://localhost:3001/",
-      }}
-      commentData={data}
-      onSubmitAction={(productDetailArray) =>
-        console.log("check submit, ", data)
-      }
-      currentData={(data) => {
-        console.log("curent data", data);
-      }}
-    />
+    <div class="card">
+      <div class="chat-header">Comments</div>
+      <div class="chat-window">
+        <ul class="message-list"></ul>
+      </div>
+      <div class="chat-input">
+        <input
+          type="text"
+          class="message-input"
+          placeholder="Type your Comment here"
+        />
+        <button class="send-button">Post</button>
+      </div>
+    </div>
   );
 };
 
